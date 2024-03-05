1124bb095456967601868ce204010fe2850df92d
gsanlop705 <gsanlop705@g.educaand.es>
2024-03-05 23:23:15 +0100

    s

--- js/q&a.js
+++ js/q&a.js
@@ -1,33 +1,28 @@
-let question = document.querySelectorAll('.question');
-let btnDropdown = document.querySelectorAll('.question .more')
-let answer = document.querySelectorAll('.answer');
-let parrafo = document.querySelectorAll('.answer p');
+let question = document.querySelectorAll(".question");
+let btnDropdown = document.querySelectorAll(".question .more");
+let answer = document.querySelectorAll(".answer");
+let parrafo = document.querySelectorAll(".answer p");
 
-for ( let i = 0; i < btnDropdown.length; i ++ ) {
+for (let i = 0; i < btnDropdown.length; i++) {
+  let altoParrafo = parrafo[i].clientHeight;
+  let switchc = 0;
 
-    let altoParrafo = parrafo[i].clientHeight;
-    let switchc = 0;
+  btnDropdown[i].addEventListener("click", () => {
+    if (switchc == 0) {
+      answer[i].style.height = `${altoParrafo}px`;
+      question[i].style.marginBottom = "10px";
+      btnDropdown[i].innerHTML = "<i>-</i>";
+      switchc++;
+    } else if (switchc == 1) {
+      answer[i].style.height = `0`;
+      question[i].style.marginBottom = "0";
+      btnDropdown[i].innerHTML = "<i>+</i>";
+      switchc--;
+    }
+  });
+}
 
-    btnDropdown[i].addEventListener('click', () => {
-
-        if ( switchc == 0 ) {
-
-            answer[i].style.height = `${altoParrafo}px`;
-            question[i].style.marginBottom = '10px';
-            btnDropdown[i].innerHTML = '<i>-</i>';
-            switchc ++;
-
-        }
-
-        else if ( switchc == 1 ) {
-
-            answer[i].style.height = `0`;
-            question[i].style.marginBottom = '0';
-            btnDropdown[i].innerHTML = '<i>+</i>';
-            switchc --;
-
-        }
-
-    })
-
+function updateBlur(value) {
+  const blurryBar = document.getElementById("blurryBar");
+  blurryBar.style.filter = `blur(${value}px)`;
 }
